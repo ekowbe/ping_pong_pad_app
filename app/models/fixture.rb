44895ffb6d9a9773652b83688aa9634f34fb9e48
paulos-t <65367687+paulos-t@users.noreply.github.com>
2021-07-13 06:15:43 -0700

    add to fixture and player model associations

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -1,4 +1,5 @@
 class Fixture < ApplicationRecord
+    has_many :matches
     has_many :fixture_teams
-    has_many :teams, through: fixture_teams
+    has_many :teams, through: :fixture_teams
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,5 @@ class Player < ApplicationRecord
     belongs_to :team
     has_many :match_players
     has_many :matches, through: :match_players
+    has_many :fixtures, through: :matches
 end
