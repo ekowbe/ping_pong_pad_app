cb0497ef0c4b920cf8373493ff57f07997f3bd31
paulos-t <65367687+paulos-t@users.noreply.github.com>
2021-07-15 08:52:14 -0700

    create player views

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,10 @@
 class PlayersController < ApplicationController
+
+    def index
+        @players = Player.all
+    end
+
+    def show
+        @player = Player.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :matches
   resources :fixtures, only: [:index, :show]
   resources :players
-  resources :teams, only: [:index, :show]
+  resources :teams
   resources :colleges
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
