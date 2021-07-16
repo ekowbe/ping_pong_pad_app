d515340a8c63e21bccef650b37bbe4cc887128ec
paulos-t <65367687+paulos-t@users.noreply.github.com>
2021-07-16 07:49:26 -0700

    final

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_113826) do
+ActiveRecord::Schema.define(version: 2021_07_16_115409) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
